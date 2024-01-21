ba150ed66c1b5b15ea62dd2c010c03504b7bfece
Newdigate <nicnewdigate@gmail.com>
2024-01-21 15:45:25 +0000

    more docking

--- extras/opengl/src/u8g2_opengl.h
+++ extras/opengl/src/u8g2_opengl.h
@@ -306,6 +306,104 @@ public:
         // Actually call in the regular Log helper (which will Begin() into the same window as we just did)
         serial_log.Draw("Serial Monitor", p_open);
     }
+
+    static void ShowDockSpace(bool* p_open)
+    {
+        // READ THIS !!!
+        // TL;DR; this demo is more complicated than what most users you would normally use.
+        // If we remove all options we are showcasing, this demo would become:
+        //     void ShowExampleAppDockSpace()
+        //     {
+        //         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+        //     }
+        // In most cases you should be able to just call DockSpaceOverViewport() and ignore all the code below!
+        // In this specific demo, we are not using DockSpaceOverViewport() because:
+        // - (1) we allow the host window to be floating/moveable instead of filling the viewport (when opt_fullscreen == false)
+        // - (2) we allow the host window to have padding (when opt_padding == true)
+        // - (3) we expose many flags and need a way to have them visible.
+        // - (4) we have a local menu bar in the host window (vs. you could use BeginMainMenuBar() + DockSpaceOverViewport()
+        //      in your code, but we don't here because we allow the window to be floating)
+
+        static bool opt_fullscreen = false;
+        static bool opt_padding = false;
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+        // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+        // because it would be confusing to have two docking targets within each others.
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+        if (opt_fullscreen)
+        {
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        }
+        else
+        {
+            dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+        }
+
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+        // and handle the pass-thru hole, so we ask Begin() to not render a background.
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+
+        // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+        // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+        // all active windows docked into it will lose their parent and become undocked.
+        // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+        // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+        if (!opt_padding)
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace Demo", p_open, window_flags);
+        if (!opt_padding)
+            ImGui::PopStyleVar();
+
+        if (opt_fullscreen)
+            ImGui::PopStyleVar(2);
+
+        // Submit the DockSpace
+        ImGuiIO& io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
+
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("Options"))
+            {
+                // Disabling fullscreen would allow the window to be moved to the front of other windows,
+                // which we can't undo at the moment without finer window depth/z control.
+                ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+                ImGui::MenuItem("Padding", NULL, &opt_padding);
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Flag: NoDockingOverCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingOverCentralNode) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingOverCentralNode; }
+                if (ImGui::MenuItem("Flag: NoDockingSplit",         "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingSplit) != 0))             { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingSplit; }
+                if (ImGui::MenuItem("Flag: NoUndocking",            "", (dockspace_flags & ImGuiDockNodeFlags_NoUndocking) != 0))                { dockspace_flags ^= ImGuiDockNodeFlags_NoUndocking; }
+                if (ImGui::MenuItem("Flag: NoResize",               "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0))                   { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
+                if (ImGui::MenuItem("Flag: AutoHideTabBar",         "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0))             { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
+                if (ImGui::MenuItem("Flag: PassthruCentralNode",    "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Close", NULL, false, p_open != NULL))
+                    *p_open = false;
+                ImGui::EndMenu();
+            }
+
+
+            ImGui::EndMenuBar();
+        }
+
+        ImGui::End();
+    }
+
     static void refreshWindow2() {
         if (!_initialized) return;
 
@@ -319,10 +417,8 @@ public:
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGuiWindowFlags window_flags = 0;
-        static bool isopen = true;
-        ImGui::Begin("docking", &isopen, window_flags);
-        ImGui::End();
+        static bool isopen = false;
+        ShowDockSpace(&isopen);
 
         {
             static float f = 0.0f;
@@ -337,21 +433,22 @@ public:
             static bool open = true;
             ShowExampleAppLog(&open);
 
-
-            ImGui::Begin("XR1-emulator!");                          // Create a window called "Hello, world!" and append into it.
-
+            ImGui::Begin("U8G2");
             //GLuint my_opengl_texture;
             //glGenTextures(1, &my_opengl_texture);
             // [...] load image, render to texture, etc.
             ImGui::Image((void*)(intptr_t)texture2, ImVec2(128,64));
+            ImGui::End();
 
-
+            ImGui::Begin("XR1 encoders");
             for (int i=0; i<5; i++) {
                 if (ImGui::SliderInt(std::string("encoder " + std::to_string(i)).c_str() , &encoderValues[i], _xr1->encoders[i].rmin, _xr1->encoders[i].rmax)){
                     _xr1->encoders[i].rval = encoderValues[i];
                 };
             }
+            ImGui::End();
 
+            ImGui::Begin("XR1 keypad");
             for (int i=0; i<6; i++) {
                 for (int j=0; j<6; j++) {
                     bool pressed = _xr1->keysPressed[i][j];
@@ -380,7 +477,9 @@ public:
                     }
                 }
             }
+            ImGui::End();
 
+            ImGui::Begin("XR1 touchpads");
             for (int i=0; i<13; i++) {
                 std::string id = std::string("T" + std::to_string(i) );
 
@@ -409,7 +508,9 @@ public:
                     ImGui::PopID();
                 }
             }
+            ImGui::End();
 
+            ImGui::Begin("XR1 leds");
             for (int i=0; i<24; i++) {
                 std::string id = std::string("L" + ((i < 10)?std::string(" "):std::string("")) + std::to_string(i) );
 
@@ -426,7 +527,6 @@ public:
                 ImGui::PopStyleColor(3);
                 ImGui::PopID();
             }
-
             ImGui::End();
         }
         ImGui::Render();
